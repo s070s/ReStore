9543c669312a076bfc5e75dfb14656882d0ab2cf
greeksotosuber <gsotiro@hotmail.com>
2024-12-06 19:46:16 +0200

    Interaction between api and frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Product } from "./product";
 
 function App() {
-  const [products, setProducts]=useState([
-    {name:'product0',price: 100.00},
-  ]);
+  const [products, setProducts]=useState<Product[]>([]);
 
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/products')
+    .then(response=>response.json())
+    .then(data => setProducts(data))
+  },[])
+  
   function addProduct(){
-    setProducts(prevState => [...prevState,{name:'product'+(prevState.length+1),price:(prevState.length*100)+100}])
+    setProducts(prevState => [...prevState,{
+      id: prevState.length+101,
+      name:'product'+(prevState.length+1),
+      price:(prevState.length*100)+100,
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200',
+
+    }])
   }
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, index) =>(
-          <li key={index}>{item.name} - {item.price}</li>
+        {products.map(product =>(
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       <button onClick={addProduct}> Add Product</button>
